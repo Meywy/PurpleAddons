d956c7e8229a142e05cd23a06bc6e22d9399b4ab
Meywy <meywyy@gmail.com>
2025-01-14 21:58:05 +0100

    cleanChat import and firstLoad text edited

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import "./features/Dungeons/General/DGGen"
 import "./features/Dungeons/Device/DGDevs"
 import "./features/Dungeons/HealerAndTank/HealerAndTank"
 import "./features/Mining/General/MiningGen"
+import "./features/Misc/CleanChat"
 
 
 // to open the config gui use the "openGUI" function
@@ -15,7 +16,7 @@ data = getDataJson
 
 if(data.data.firstLoad){
     setTimeout(() => {
-        ChatLib.chat(`\n${prefix.replace(">", "")}\n&aThanks for using PurpleAddons!\n&aIf you have any suggestions or bugs, please open issue request on github\n&aUsage: /pa`)
+        ChatLib.chat(`\n${prefix.replace(">", "")}\n&aThanks for using PurpleAddons!\n&aIf you have any suggestions or bugs, please open issue request on github\n&aUsage: /pa\n`)
         data.data.firstLoad = false
         data.save()
     }, 300)
