69ac41e0d2e5eed7c44ccd85a4784c6ed985f7e8
Meywy <meywyy@gmail.com>
2025-01-15 22:28:03 +0100

    eeGui added to data, edited prefix2

--- utils/Utils.js
+++ utils/Utils.js
@@ -1,8 +1,8 @@
 import { registerWhen } from "../../BloomCore/utils/Utils";
-import PogObject from "../../PogData"
+import PogObject from "../../PogData";
 
 export const prefix = "&5PurpleAddons &a>&r"
-export const prefix2 = "PurpleAddons >"
+export const prefix2 = "[PurpleAddons]"
 export const line = "&m-".repeat(ChatLib.getChatWidth() / 6);
 
 // Credit: UA
@@ -17,5 +17,10 @@ registerWhen(register("step", () => {
 export const getDataJson = new PogObject('PurpleAddons', {
     data: {
         firstLoad: true
+    },
+    eeGui: {
+        x: Renderer.screen.getWidth() / 2,
+        y: Renderer.screen.getHeight() / 2,
+        scale: 1
     }
 }, "data.json")
