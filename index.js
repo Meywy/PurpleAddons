eae554f7ec95ffe646b2f0d706586b3cef28cbff
Meywy <meywyy@gmail.com>
2025-02-01 14:53:01 +0100

    changed messages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Settings from "./config";
 import axios from "../axios";
-import { getDataJson, prefix } from "./utils/Utils";
+import { getDataJson, prefix, line } from "./utils/Utils";
 
 // features
 import "./features/Dungeons/General/DGGen"
@@ -12,15 +12,18 @@ import "./features/Misc/CleanChat"
 import "./features/Dungeons/M7/Rag"
 import "./features/Dungeons/General/LeapAnnouc"
 import "./features/Dungeons/M7/Crystal"
+import "./features/Dungeons/M7/ArrowStack"
+import "./features/Dungeons/M7/DebuffWaypoints"
 
 // register the commands
 import "./features/Commands/pearls"
-
-
-register("command", () => Settings.openGUI()).setName("purpleaddons").setAliases("pa").setAliases("purpa").setAliases("padds").setAliases("paddons");
+import "./features/Commands/leaps"
 
 data = getDataJson
 const version = JSON.parse(FileLib.read("PurpleAddons", "metadata.json")).version
+let updateNoti = false
+
+register("command", () => Settings.openGUI()).setName("purpleaddons").setAliases("pa").setAliases("purpa").setAliases("padds").setAliases("paddons");
 
 if(data.data.firstLoad){
     setTimeout(() => {
@@ -31,31 +34,32 @@ if(data.data.firstLoad){
     }, 300)
 }
 
-let ctNoti = false
 register("worldLoad", () => {
-    if (ctNoti) return
+    if (updateNoti) return
     axios.get(`https://chattriggers.com/api/modules/purpleaddons`)
     .then(res => {
-        let changes = res.data.releases[0].changelog.toString().replaceAll("\r", "")
         const newVer = res.data.releases[0].releaseVersion
         if (data.data.last_version !== version) {
             if (data.data.last_version === undefined) return
-            ChatLib.chat(`\n${prefix}\n&5PurpAdds has updated! &ev${data.data.last_version} ➜ v${version}`)
+            ChatLib.chat(`\n${line}`)
+            ChatLib.chat(ChatLib.getCenteredText(prefix))
+            ChatLib.chat(`${line}\n&aUpdate completed! &ev${data.data.last_version} -> v${version}`)
             data.data.last_version = version
             data.save()
-            ctNoti = true;
+            updateNoti = true;
         } else {
             // NWJN || https://www.chattriggers.com/modules/v/NwjnAddons
                 if (newVer == version) return;
-                if (version.includes("pre")) return;
-                
                 if (newVer != version) {
-                    ChatLib.chat(`\n${prefix}\n&aPurpAdds has new update! &2v${version} ➜ v${newVer}\n&eChangelog:\n${changes}`)
-                    new TextComponent(`&a&lClick here to update!\n`)
+                    ChatLib.chat(`\n${line}`)
+                    ChatLib.chat(ChatLib.getCenteredText(prefix))
+                    ChatLib.chat(line)
+                    ChatLib.chat(ChatLib.getCenteredText(`&aNew update! &2v${version} -> v${newVer}`))
+                    new TextComponent(ChatLib.getCenteredText(`&a&lClick here to update!\n`))
                     .setClickAction("run_command")
                     .setClickValue(`/ct load`)
                     .chat()
-                    ctNoti = true;
+                    updateNoti = true;
                 }
             //
         }
